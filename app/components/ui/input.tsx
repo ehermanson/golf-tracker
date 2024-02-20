ab20fa6c917a49e6532ab6c04caa9e30e125e0f9
Eric Hermanson <ehermanson@wayfair.com>
2024-02-19 22:07:02 -0500

    disable wheel events on number inputs

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -1,3 +1,4 @@
+import { mergeRefs } from 'react-merge-refs';
 import * as React from 'react';
 
 import { cn } from '~/utils';
@@ -8,6 +9,26 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
 	({ className, type = 'text', ...props }, ref) => {
+		const inputRef = React.useRef<HTMLInputElement>();
+
+		// Disable wheel event from triggering step change on input
+		React.useEffect(() => {
+			if (type !== 'number') {
+				return;
+			}
+			const { current: input } = inputRef;
+
+			const handleWheel = (ev: Event) => {
+				if ((ev as WheelEvent).deltaY && document.activeElement === input) {
+					ev.preventDefault();
+				}
+			};
+
+			input?.addEventListener('wheel', handleWheel);
+
+			return () => input?.removeEventListener('wheel', handleWheel);
+		}, [type]);
+
 		return (
 			<input
 				type={type}
@@ -15,7 +36,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 					'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 aria-[invalid]:border-red-600',
 					className,
 				)}
-				ref={ref}
+				ref={mergeRefs([ref, inputRef])}
 				{...props}
 			/>
 		);
